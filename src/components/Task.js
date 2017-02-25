ed2e36fc3886e4f312131ab51428a46b5f44a932
Austin Baltes <austinba@gmail.com>
2017-02-25 04:32:22 -0500

    added a corner triangle

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ISBox from './ISBox';
+import _ from 'lodash/fp';
 import '../styles/tasks.css';
 
 const Task = props => {
   const { taskID } = props;
   const task = props.tasks[taskID];
+  const comments = task.comments;
+
+  const commentBoxes = _.map(comment => (
+    <div className="box task-comment">
+      {comment.from}: {comment.comment} - 1 hr ago
+    </div>
+  ))(comments);
 
   return (
     <div className="box task-container">
       <div className="main-task-box">
+        <svg width="50" height="50" className="corner-working-on-marker">
+          <path d="M 0,0 L 50,50 L 50,0 Z" />
+        </svg>
         <ISBox level={task.importanceSeverity} dateDue={task.dateDue} />
         <div className="task-description">
           {task.description}
@@ -21,9 +32,7 @@ const Task = props => {
           Delete&nbsp;&nbsp;-&nbsp;&nbsp;Edit&nbsp;&nbsp;-&nbsp;&nbsp;Mark&nbsp;complete
         </span>
       </div>
-      <div className="box task-comments">
-        These are some comments
-      </div>
+      {commentBoxes}
     </div>
   );
 };
